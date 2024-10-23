d3b9044dfdc507b612f1f4c1561a7b9f3f3ef0da
Azeem Yousaf <develop22.coders@gmail.com>
2024-10-23 07:25:58 +0000

    Update

--- JS/test.js
+++ JS/test.js
@@ -1,19 +1,29 @@
 // Execution
-console.log(10+98);
+let exc = 10+98;
+console.log(exc);
+console.log(typeof exc);
 
 // >>>>>>>> Data Types <<<<<<<<<<<<<<<<<  //
 
 // 1 Numbers (0,1,2,3,4,5,6,7,8,9....)
+
 console.log (95);
+console.log (typeof 95);
 
 // 2 Strings ( Any Text, "abcd", "Any Name" etc)
 console.log("World");
+console.log(typeof "World");
 
 // 3 Boolean ( True , False )
-console.log( 10 = 01)
+console.log( 10 == 1 )
+console.log(typeof (10 == 1) );
 
 // 4 Undefined ( Undefined means there is no defined value)
-
+let undefi;
+console.log(undefi)
+console.log(typeof undefi)
 
 // 5 Null ( Null means there is not a value but it does not means that is zero ) 
-
+let nulll ="";
+console.log(nulll)
+console.log(typeof nulll)
