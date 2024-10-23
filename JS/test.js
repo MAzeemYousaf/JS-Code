16930af8a8a4480aeb7ea268fd5bfcc77ea629f4
Azeem Yousaf <develop22.coders@gmail.com>
2024-10-23 10:49:45 +0500

    Update test.js

--- JS/test.js
+++ JS/test.js
@@ -1,1 +1,19 @@
+// Execution
 console.log(10+98);
+
+// >>>>>>>> Data Types <<<<<<<<<<<<<<<<<  //
+
+// 1 Numbers (0,1,2,3,4,5,6,7,8,9....)
+console.log (95);
+
+// 2 Strings ( Any Text, "abcd", "Any Name" etc)
+console.log("World");
+
+// 3 Boolean ( True , False )
+console.log( 10 = 01)
+
+// 4 Undefined ( Undefined means there is no defined value)
+
+
+// 5 Null ( Null means there is not a value but it does not means that is zero ) 
+
