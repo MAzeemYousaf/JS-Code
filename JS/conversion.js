395566f725b1c7eafbca86e1d4c257949d6f9739
Azeem Yousaf <develop22.coders@gmail.com>
2024-10-23 07:25:29 +0000

    Conversion

---
+++ JS/conversion.js
@@ -0,0 +1,16 @@
+// >>>>>>>>>>>>>>>>>>>>>>   TYPE CONVERSION <<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+let number = 22;
+console.log(number);
+console.log(typeof number);
+
+let stringNum = "22";
+console.log(typeof stringNum);
+console.log(stringNum);
+
+
+// converting Data type string into Number
+
+let stringNumber = Number(stringNum);
+console.log(typeof stringNumber);
+console.log(stringNumber);
